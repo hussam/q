5fb68fba1ab1d6a75b28896ac0e760c093583c75
Hussam Abu-Libdeh <hussam@0xff.co>
2015-03-25 22:18:07 -0400

    Added basic DB to qlib

---
+++ lib/DB.fs
@@ -0,0 +1,32 @@
+﻿namespace qlib
+
+open System
+open System.Threading.Tasks
+open System.Collections.Generic
+open SQLite
+
+type DBItem() = 
+    [<PrimaryKey; AutoIncrement>]
+    member val LocalId :int = 0 with get, set
+
+type QItem() =
+    inherit DBItem()
+    member val Text : string = null with get, set
+    member val Hashtag : string = null with get, set
+    member val VenueId : string = null with get, set
+    member val PhotoUrl : string = null with get, set
+
+type QItemDB(path) =
+    inherit SQLiteAsyncConnection(path)
+
+    let locker : obj = new obj()
+
+    member this.CreateTable() =
+        lock locker (fun() -> this.CreateTableAsync<QItem>()) |> Async.AwaitTask
+
+    member this.GetItems() =
+        let table = lock locker ( fun() -> this.Table<QItem>() )
+        table.ToListAsync() |> Async.AwaitTask
+
+    member this.SaveItem(item : QItem) =
+        lock locker (fun() -> this.InsertAsync(item)) |> Async.AwaitTask
---
+++ lib/Script.fsx
@@ -0,0 +1,16 @@
+﻿#I "/Users/hussam/projects/q/lib/bin/Debug/"
+#r "SQLite.dll"
+#load "DB.fs"
+
+open qlib
+
+let db = new QItemDB("/Users/hussam/tmp/testing.sqlite")
+
+do db.CreateTable() |> ignore
+
+let mutable q = new QItem()
+db.SaveItem(q) |> ignore
+
+q.Text <- "Some activitiy"
+q.Hashtag <- "Testing"
+db.SaveItem(q)
