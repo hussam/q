8adeb20bc2d93806c3deb10a4ff5d1fd1dd5409a
Hussam Abu-Libdeh <hussam@0xff.co>
2015-03-29 20:43:38 -0400

    Modified QLib to use local copy of Xamarin.iOS.dll in order to simplify testing with Script.fsx

--- lib/Script.fsx
+++ lib/Script.fsx
@@ -1,8 +1,15 @@
 ﻿#I "/Users/hussam/projects/q/lib/bin/Debug/"
+#r "Xamarin.iOS.dll"
 #r "SQLite.dll"
 #load "DB.fs"
 
+// NOTE: This project uses a local copy of Xamarin.iOS.dll in order to enable Script testing.
+// I don't know if this is good or bad. Consider removing local copy.
+
+
 open qlib
+open System
+
 
 let db = new QItemDB("/Users/hussam/tmp/testing.sqlite")
 
@@ -14,3 +21,7 @@ db.SaveItem(q) |> ignore
 q.Text <- "Some activitiy"
 q.Hashtag <- "Testing"
 db.SaveItem(q)
+
+let x = nfloat 32.0f
+
+
