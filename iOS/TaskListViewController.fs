193cf0632eabe8da9ae578813fa35700bcd03265
Hussam Abu-Libdeh <hussam@0xff.co>
2015-05-22 15:39:38 -0400

    Rebind cells correctly after state change in TaskListViewController

--- iOS/TaskListViewController.fs
+++ iOS/TaskListViewController.fs
@@ -23,10 +23,64 @@ type QueueCell =
 
     static member ReuseId : string = "QueueCell"
 
-    member this.Bind (item : QItem) =
+    member this.Bind (item : QItem, swipeMode) =
         this.item <- item
         this.TextLabel.Text <- item.Text
         this.TextLabel.Enabled <- not item.Completed
+
+        this.CellSwipeGestureRecognizer.ShortTrigger <- nfloat 0.30
+        let frameWithWidth width = CGRectWithSize width 60.0
+
+        if item.Completed = false then
+            let completedActionView = new UILabel(frameWithWidth 100.0)
+            completedActionView.Text <- "Completed"
+            completedActionView.TextColor <- UIColor.White
+            completedActionView.TextAlignment <- UITextAlignment.Center
+            this.SetSwipeGestureWithView(
+                completedActionView,
+                UIColor.QGreen,
+                swipeMode,
+                SwipeTableViewCellState.StateRightShort,
+                new SwipeCompletionBlock(fun view state mode ->
+                    QLib.MarkItemAsCompleted(item)
+                    this.Bind(item, swipeMode)
+                    Insights.Track("CompletedTask")
+                    )
+                )
+        else
+            let uncompletedAction = new UILabel(frameWithWidth 110.0)
+            uncompletedAction.Text <- "Uncomplete"
+            uncompletedAction.TextColor <- UIColor.White
+            uncompletedAction.TextAlignment <- UITextAlignment.Center
+            this.SetSwipeGestureWithView(
+                uncompletedAction,
+                UIColor.QBlue,
+                swipeMode,
+                SwipeTableViewCellState.StateLeftShort,
+                new SwipeCompletionBlock(fun view state mode ->
+                    QLib.MarkItemAsUncompleted(item)
+                    this.Bind(item, swipeMode)
+                    Insights.Track("UnCompletedTask")
+                    )
+                )
+
+        #if false
+        let deleteActionView = new UILabel(frameWithWidth 80.0)
+        deleteActionView.Text <- "Delete"
+        deleteActionView.TextColor <- UIColor.White
+        deleteActionView.TextAlignment <- UITextAlignment.Center
+        cell.SetSwipeGestureWithView(
+            deleteActionView,
+            UIColor.QMagenta,
+            SwipeTableCellMode.Exit,
+            SwipeTableViewCellState.StateLeftShort,
+            new SwipeCompletionBlock(fun view state mode ->
+                QLib.DeleteItem(item)
+                Insights.Track("DeletedTask", "topic", item.Topic)
+                )
+            )
+        #endif
+
 
 
 type QueueViewSource(queueType, tableView : UITableView, taskSelected) =
@@ -56,65 +110,10 @@ type QueueViewSource(queueType, tableView : UITableView, taskSelected) =
     override this.GetCell(tableView, indexPath) =
         let item = itemAt indexPath
         let cell = tableView.DequeueReusableCell (QueueCell.ReuseId, indexPath) :?> QueueCell
-        cell.Bind(item)
-
-        cell.CellSwipeGestureRecognizer.ShortTrigger <- nfloat 0.30
-        let frameWithWidth width = CGRectWithSize width 60.0
-
         let swipeMode = match queueType with
                         | AllTasks -> SwipeTableCellMode.Switch
                         | _ -> SwipeTableCellMode.Exit
-
-        if item.Completed = false then
-            let completedActionView = new UILabel(frameWithWidth 100.0)
-            completedActionView.Text <- "Completed"
-            completedActionView.TextColor <- UIColor.White
-            completedActionView.TextAlignment <- UITextAlignment.Center
-            cell.SetSwipeGestureWithView(
-                completedActionView,
-                UIColor.QGreen,
-                swipeMode,
-                SwipeTableViewCellState.StateRightShort,
-                new SwipeCompletionBlock(fun view state mode ->
-                    QLib.MarkItemAsCompleted(item)
-                    cell.TextLabel.Enabled <- false
-                    Insights.Track("CompletedTask")
-                    )
-                )
-        else
-            let uncompletedAction = new UILabel(frameWithWidth 110.0)
-            uncompletedAction.Text <- "Uncomplete"
-            uncompletedAction.TextColor <- UIColor.White
-            uncompletedAction.TextAlignment <- UITextAlignment.Center
-            cell.SetSwipeGestureWithView(
-                uncompletedAction,
-                UIColor.QBlue,
-                swipeMode,
-                SwipeTableViewCellState.StateLeftShort,
-                new SwipeCompletionBlock(fun view state mode ->
-                    QLib.MarkItemAsUncompleted(item)
-                    cell.TextLabel.Enabled <- true
-                    Insights.Track("UnCompletedTask")
-                    )
-                )
-
-        #if false
-        let deleteActionView = new UILabel(frameWithWidth 80.0)
-        deleteActionView.Text <- "Delete"
-        deleteActionView.TextColor <- UIColor.White
-        deleteActionView.TextAlignment <- UITextAlignment.Center
-        cell.SetSwipeGestureWithView(
-            deleteActionView,
-            UIColor.QMagenta,
-            SwipeTableCellMode.Exit,
-            SwipeTableViewCellState.StateLeftShort,
-            new SwipeCompletionBlock(fun view state mode ->
-                QLib.DeleteItem(item)
-                Insights.Track("DeletedTask", "topic", item.Topic)
-                )
-            )
-        #endif
-
+        cell.Bind(item, swipeMode)
         cell :> UITableViewCell
 
 
