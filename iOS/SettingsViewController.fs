2f1185242394c968d93014ac468b27b3e0434ff3
Hussam Abu-Libdeh <hussam@0xff.co>
2015-05-23 03:32:56 -0400

    Prelimenary settings page

--- iOS/SettingsViewController.fs
+++ iOS/SettingsViewController.fs
@@ -4,15 +4,32 @@ open System
 open UIKit
 open Foundation
 
+open Praeclarum.AutoLayout
+
 type SettingsViewController() as this =
     inherit UIViewController()
     do
+        this.EdgesForExtendedLayout <- UIRectEdge.None
         this.Title <- "Settings"
 
     override this.ViewDidLoad() =
         base.ViewDidLoad()
+
+        let name = new StyledTextField()
+        name.Placeholder <- "Name"
+
+        let email = new StyledTextField()
+        email.Placeholder <- "Email"
+
         let view = this.View
         view.BackgroundColor <- UIColor.White
-        let label = new UILabel(view.Bounds)
-        label.Text <- "TBD"
-        view.AddSubview(label)
+        view.AddSubviews(name, email)
+        view.AddConstraints [|
+                name.LayoutTop == view.LayoutTop + nfloat 20.0
+                name.LayoutLeft == view.LayoutLeft + nfloat 20.0
+                name.LayoutWidth == view.LayoutWidth - nfloat 40.0
+
+                email.LayoutTop == name.LayoutBottom + nfloat 20.0
+                email.LayoutLeft == name.LayoutLeft
+                email.LayoutWidth == name.LayoutWidth
+            |]
